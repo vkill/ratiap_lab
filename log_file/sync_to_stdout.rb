c6c49c833a64d212c3fcbadb5c6446bd5f9371c5
vkill <vkill.net@gmail.com>
2016-05-10 12:26:49 +0800

    add io/wait to log_file sync_to_stdout lab

--- log_file/sync_to_stdout.rb
+++ log_file/sync_to_stdout.rb
@@ -2,6 +2,8 @@
 
 require 'tmpdir'
 require 'fileutils'
+require 'io/wait'
+
 require 'pry'
 
 log_file = File.join(Dir.tmpdir, 'test.log')
@@ -9,8 +11,10 @@ log_file = File.join(Dir.tmpdir, 'test.log')
 FileUtils.mkdir_p File.dirname(log_file)
 FileUtils.touch log_file
 FileUtils.chmod 0777, log_file
+File.truncate(log_file, 0)
 
 writer = File.open(log_file, 'r')
+writer.wait_readable
 
 while true
   if line = writer.gets
